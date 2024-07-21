c77d1d301680c962b59fc239edfee8e9b5e5d0d6
Maysam AlKhusaibi <149917210+May-1965@users.noreply.github.com>
2024-07-21 22:38:08 +0400

    Update problem_2.py

--- problem_2.py
+++ problem_2.py
@@ -1,7 +1,7 @@
 def decimal_to_binary(n):
     return bin(n)[2:]  
 
-###################################
+#######################################
 while True:
     try:
         number = int(input("Enter a positive decimal number: "))
@@ -9,7 +9,7 @@ while True:
         if number < 0:
             print("Invalid input! please enter a positive number")
         else:
- ####################################
+ ########################################
             
             binary_equivalent = decimal_to_binary(number)
             print(f"The binary equivalent is {binary_equivalent}")
